7cff4a0a67e3272bd5f06dceda24fd58e6119a74
ozwaldorf <self@ossian.dev>
2023-09-07 23:23:48 -0400

    crossbeam and atomic counter

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
-use std::{io::Write, sync::mpsc::sync_channel, time::Instant};
+use std::{
+    io::Write,
+    sync::{
+        atomic::{AtomicUsize, Ordering},
+        Arc,
+    },
+    time::Instant,
+};
 
 use clap::Parser;
 use fastcrypto::{
@@ -18,11 +25,13 @@ struct Args {
 fn main() {
     let args = Args::parse();
 
-    let (tx, rx) = sync_channel(256);
+    let (tx, rx) = crossbeam_channel::bounded(256);
+    let counter = Arc::new(AtomicUsize::default());
 
     // Spawn key generation threads
     for _ in 0..args.threads {
         let tx = tx.clone();
+        let counter = counter.clone();
         std::thread::spawn(move || {
             let mut rng = ThreadRng::default();
             loop {
@@ -32,28 +41,28 @@ fn main() {
                     pair.private().as_bytes().to_vec(),
                 ))
                 .expect("failed to send tx");
+                counter.fetch_add(1, Ordering::Relaxed);
             }
         });
     }
 
     let mut timer = Instant::now();
-    let mut count = 0;
     let prefix = args.prefix.to_lowercase();
     let mut stdout = std::io::stdout().lock();
 
     // Process keys
     while let Ok((key, secret)) = rx.recv() {
         if timer.elapsed().as_secs() >= 1 {
+            let count = counter.load(Ordering::Relaxed);
             write!(stdout, "\r\x1b[K{} keys/second", count).unwrap();
             stdout.flush().unwrap();
             timer = Instant::now();
-            count = 0;
+            counter.fetch_sub(count, Ordering::Relaxed);
         }
 
         if key.to_lowercase().starts_with(&prefix) {
             let secret = Base64::from_bytes(&secret).encoded();
             println!("\r\x1b[KFound:  {key}\n  Key:  {secret}");
         }
-        count += 1;
     }
 }
