255b07467b640325173f499c64cda395d3a90fec
Parsa Ghadimi <i@parsa.ooo>
2023-09-07 23:30:09 -0400

    move check to worker

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::{
         atomic::{AtomicUsize, Ordering},
         Arc,
     },
-    time::Instant,
+    time::{Duration, Instant},
 };
 
 use clap::Parser;
@@ -27,42 +27,45 @@ fn main() {
 
     let (tx, rx) = crossbeam_channel::bounded(256);
     let counter = Arc::new(AtomicUsize::default());
+    let prefix = args.prefix.to_lowercase();
 
     // Spawn key generation threads
     for _ in 0..args.threads {
         let tx = tx.clone();
         let counter = counter.clone();
+        let prefix = prefix.clone();
+
         std::thread::spawn(move || {
             let mut rng = ThreadRng::default();
             loop {
                 let pair = Ed25519KeyPair::generate(&mut rng);
-                tx.send((
-                    Base64::from_bytes(pair.public().as_bytes()).encoded(),
-                    pair.private().as_bytes().to_vec(),
-                ))
-                .expect("failed to send tx");
+                let pk = Base64::from_bytes(pair.public().as_bytes()).encoded();
+
+                if pk.to_lowercase().starts_with(&prefix) {
+                    tx.send((pk, pair.private().as_bytes().to_vec()))
+                        .expect("failed to send tx");
+                }
+
                 counter.fetch_add(1, Ordering::Relaxed);
             }
         });
     }
 
     let mut timer = Instant::now();
-    let prefix = args.prefix.to_lowercase();
     let mut stdout = std::io::stdout().lock();
 
-    // Process keys
-    while let Ok((key, secret)) = rx.recv() {
+    loop {
+        if let Ok((pk, secret)) = rx.recv_timeout(Duration::from_millis(100)) {
+            let secret = Base64::from_bytes(&secret).encoded();
+            println!("\r\x1b[KFound:  {pk}\n  Key:  {secret}");
+        }
+
         if timer.elapsed().as_secs() >= 1 {
             let count = counter.load(Ordering::Relaxed);
             write!(stdout, "\r\x1b[K{} keys/second", count).unwrap();
             stdout.flush().unwrap();
             timer = Instant::now();
             counter.fetch_sub(count, Ordering::Relaxed);
-        }
-
-        if key.to_lowercase().starts_with(&prefix) {
-            let secret = Base64::from_bytes(&secret).encoded();
-            println!("\r\x1b[KFound:  {key}\n  Key:  {secret}");
         }
     }
 }
